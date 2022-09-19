1ad7759b7f7f6f6e9571e92182edf0cc51ab8576
Angus02 <angushill02@gmail.com>
2022-09-19 16:16:55 +0100

    added coming soon page

---
+++ gooses/src/components/pages/ComingSoon.js
@@ -0,0 +1,33 @@
+import {React} from 'react'
+import '../Geese.css'
+import { Button } from '../Button'
+
+
+function ComingSoon() {
+    
+    return (
+        <div className='fill'>
+        
+                <div className='container'>
+        
+                    <div className='containerBlackFill'>
+                        <div className='centered' >
+
+                            <Button buttonStyle='btn--outline' buttonSize={'btn--large'}>
+                                    <span>Coming Soon</span>
+                            </Button>
+
+                            </div>
+                        <div className='centreRight'>
+                            <div className='list' ></div>
+                            <div className='list' ></div>
+                            <div className='list' ></div>
+                            <div className='list' ></div>
+                        </div>
+                    </div>
+                </div>
+        </div>
+    );
+}
+
+export default ComingSoon;
